70d1aa24f928e197ed83cbf50f6bc52a11ca179c
Sushant <34920404+Sushant-ABdigital@users.noreply.github.com>
2018-07-14 15:44:37 -0600

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+pannellum.viewer('panorama', {
+  // "type": "equirectangular",
+  "panorama": "http://i.imgur.com/Xfbh2k9.jpg",
+  "autoLoad": true,
+  "autoRotate": -1,
+  "showZoomCtrl": false,
+  "showFullscreenCtrl": false,
+  "mouseZoom": false
+});
